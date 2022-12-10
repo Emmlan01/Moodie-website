8b628be41e270855ebfbb4f21e75f11c1990fcbb
gartmon <gartmon@kth.se>
2022-12-10 14:32:01 +0100

    la in lite saker3

--- src/App.js
+++ src/App.js
@@ -20,3 +20,34 @@ function App(props) {
     </div>
    );
 }
+
+
+/*import React from "react";
+import './App.css';
+import { getMovieDetails } from './movieSource.js'
+import { getCurrentWeather } from './weatherSource.js'
+import  Login  from './reactjs/loginPresenter.js';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; //third component
+import  Register  from './reactjs/registerPresenter.js';
+import  ForgotPassword  from './reactjs/forgotPasswordPresenter.js';
+
+
+const Movie=require("./reactjs/moviePresenter.js").default;
+const Weather=require("./reactjs/weatherPresenter.js").default;
+
+
+export default
+function App(props) {
+
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </div>
+   );
+}*/
