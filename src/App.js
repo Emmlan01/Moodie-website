b99af14c274619a6e1982b354eb395b0a5ef8d66
Mattias Ritze <mattias.ritze@gmail.com>
2022-12-14 18:22:50 +0100

    Revert "removed react-helmet"
    This reverts commit dbee8c5942496a6c731b714e8a3b55c6817f9486.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { getMovieDetails } from './movieSource.js'
 import MovieModel from './movieModel';
 import { getCurrentWeather } from './weatherSource.js'
 import React from "react";
+import {Helmet} from "react-helmet";
 
 const Movie=require("./reactjs/moviePresenter.js").default;
 const Weather=require("./reactjs/weatherPresenter.js").default;
