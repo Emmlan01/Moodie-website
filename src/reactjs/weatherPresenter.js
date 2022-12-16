59392a08395927d590abdf9c1c62bf919815102e
unknown <wig@kth.se>
2022-12-16 23:40:48 +0100

    added some documentation and removed a bunch of old, useless stuff.

--- src/reactjs/weatherPresenter.js
+++ src/reactjs/weatherPresenter.js
@@ -1,8 +1,6 @@
 import promiseNoData from "../views/promiseNoData"
 import WeatherView from "../views/weatherView.js"
 import { useState, useEffect } from "react";
-import { determineWeather} from "../utilities.js"
-import Movie from "./moviePresenter";
 
 export default function Weather(props) {
     // This is used to make the component stateful. The upper useState() sets an empty moviePromiseState. The lower doesn't store any state information but is
@@ -10,32 +8,21 @@ export default function Weather(props) {
     const[weatherPromiseState] = useState({});
     const[, weatherPromiseStateChanged] = useState();
 
-    function getWeatherPromiseState(){
-        console.log("hello there")
-        //return weatherPromiseState;
-    }
-
-    // Used to do an initial get when the Movie component is created.
+    // Used to do an initial get when the weather component is created.
     useEffect(initialGetCurrentWeather, []);
 
-    // This callback function is used to do an initial getCurrentMovieDetails, updating the moviePromiseState.
-    // This should most likely be deprecated later on.
     function initialGetCurrentWeather(){
-        // Testing this with a temporary id of 500. This should of course be dynamically changeable in the final app.
-
         props.model.currentWeatherID=weatherPromiseState;
         props.model.getCurrentWeather(props.model.userLatitude, props.model.userLongitude,  weatherPromiseState, notify)
 
     }
 
-    //console.log("weather props", JSON.stringify(weatherPromiseState.data.lon))
-
-    // This function is used to notify React of changes in the moviePromiseState. It is called 
+    // This function is used to notify React of changes in the weatherPromiseState.
     function notify(){
         weatherPromiseStateChanged(new Object());
     }
 
-    // Returns either the promiseNoData function (no data, spinner image, etc) or the content defined in the MovieView.
+    // Returns either the promiseNoData function (no data, spinner image, etc) or the content defined in the weather view.
     return (<div>{promiseNoData(weatherPromiseState) || <WeatherView weatherData={weatherPromiseState.data}/>}</div>)
 
 }
