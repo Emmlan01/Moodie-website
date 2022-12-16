a96a448cd4e7341fb7ee5ab674204d44789d1ed1
unknown <wig@kth.se>
2022-12-16 23:08:51 +0100

    use user location from start, goodbye Amores Perros!

--- src/reactjs/moviePresenter.js
+++ src/reactjs/moviePresenter.js
@@ -3,10 +3,7 @@ import MovieView from "../views/movieView.js"
 import { useState, useEffect } from "react";
 import { rerollMovie } from "../movieSource";
 
-import { getMovieGenres } from "../movieSource";
-
 export default function Movie(props) {
-
     // This is used to make the component stateful. The upper useState() sets an empty moviePromiseState. The lower doesn't store any state information but is
     // solely used to force re-renders upon calls to the moviePromiseStateChanged function (See lecture slide 119 - 135 for further explanations of what is going on here).
     const[moviePromiseState] = useState({});
@@ -18,12 +15,7 @@ export default function Movie(props) {
     // This callback function is used to do an initial getCurrentMovieDetails, updating the moviePromiseState.
     // This should most likely be deprecated later on.
     function initialGetCurrentMovieDetails(){
-        // Testing this with a temporary id of 500. This should of course be dynamically changeable in the final app.
-
         rerollMovieACB();
-        //props.model.getCurrentMovieDetails(55, moviePromiseState, notify);
-        
-
     }
 
     // This function is used to notify React of changes in the moviePromiseState, triggering a re-render of the component.
@@ -31,17 +23,13 @@ export default function Movie(props) {
         moviePromiseStateChanged(new Object());
     }
 
-    // *TODO* - call a function which generates a new movie ID
+    // This function is used to reroll
     async function rerollMovieACB(){
-        //console.log(props.model.currentWeatherID.data.weather[0].id);
-        
-        //await props.model.getCurrentMovieDetails(100, moviePromiseState, notify);
-        //console.log("there ya go");
-        const selectables = await rerollMovie(37);                              // This is used to do a call to find the amount of selectable pages of movies
-        const page =  Math.floor(Math.random() * selectables.total_pages);      // This is used to select a page.
-        const selectableMovies = await rerollMovie(37, page);                   // This gets the selected page
-        const movie = selectableMovies.results[Math.floor(Math.random() * 20)];
-        props.model.getCurrentMovieDetails(movie.id, moviePromiseState, notify);
+        const selectables = await rerollMovie(37);                                  // This is used to do a call to find the amount of selectable pages of movies.
+        const page =  Math.floor(Math.random() * selectables.total_pages);          // This is used to select a page.
+        const selectableMovies = await rerollMovie(37, page);                       // This gets the selected page.
+        const movie = selectableMovies.results[Math.floor(Math.random() * 20)];     // This generates a movie ID from one of the fetched movies.
+        props.model.getCurrentMovieDetails(movie.id, moviePromiseState, notify);    // This finally calls getCurrentMovieDetails with the aqquired movie ID.
         
     }
 
